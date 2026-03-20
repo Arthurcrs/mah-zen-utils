14c980094e3768e704cd51f06c5396fe2ab32650
Arthur Carlos <arthcrs@gmail.com>
2026-03-19 22:50:34 -0300

    refactoring

---
+++ src/main/java/com/mahghuuuls/mahghuuulszenutils/MahZenUtils.java
@@ -0,0 +1,11 @@
+package com.mahghuuuls.mahghuuulszenutils;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = MahZenUtils.MOD_ID, name = MahZenUtils.NAME, version = MahZenUtils.VERSION, dependencies = MahZenUtils.DEPENDENCIES)
+public class MahZenUtils {
+	public static final String MOD_ID = "mahzenutils";
+	public static final String NAME = "Mah Zen Utils";
+	public static final String VERSION = "0.1.0";
+	public static final String DEPENDENCIES = "";
+}
